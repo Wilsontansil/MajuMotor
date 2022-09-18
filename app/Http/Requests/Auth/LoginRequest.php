871b1caab92d604daab2d9ef093a71be12c82906
wilsontansil <tanwilson2410@gmail.com>
2022-09-18 14:50:01 +0700

    Fix Auth Login Username

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -49,7 +49,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'username' => trans('auth.failed'),
+                'username' => __('Username dan password tidak sesuai.'),
             ]);
         }
 
